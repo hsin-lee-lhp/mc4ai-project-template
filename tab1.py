15f84b26c82623f4146f10aeb09aa521d63ec51e
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-10 22:48:00 +0700

    Update tab1.py

--- tab1.py
+++ tab1.py
@@ -48,18 +48,4 @@ def score():
         histogram(df,'GPA',None,'PYTHON-CLASS')
         histogram(df,'GPA',None,'GENDER')
         pie(df[df['GPA'] >= 6],'CLASS-GROUP','Tỉ lệ đậu lớp MC')
-def bieu_do():
-    a_names = ('PHÂN BỐ HS NAM - NỮ','PHÂN BỐ HS LỚP SÁNG - CHIỀU','PHÂN BỐ HS CHUYÊN - THƯỜNG','PHÂN BỐ HS CÁC KHỐI')
-    a = st.radio('Loại biểu đồ',a_names,horizontal=True)
-    if a=='PHÂN BỐ HS NAM - NỮ':
-        pie(df, df["GENDER"],"PHÂN BỐ HS NAM - NỮ")
-        st.write('Nam hứng thú với AI hơn')
-    if a=='PHÂN BỐ HS LỚP SÁNG - CHIỀU':
-        pie(df, df["PYTHON-CLASS"],"PHÂN BỐ HS LỚP SÁNG CHIỀU")
-        st.write('Học sinh thường chọn học AI vào buổi sáng')
-    if a=='PHÂN BỐ HS CHUYÊN - THƯỜNG':
-        pie(df, df["CLASS-GROUP"],"PHÂN BỐ HS CHUYÊN - THƯỜNG")
-        st.write('Học sinh lớp chuyên có hứng thú với AI hơn')
-    if a=='PHÂN BỐ HS CÁC KHỐI':
-        pie(df, df["GRADE"],"PHÂN BỐ HS CÁC KHỐI")
-        st.write("Học sinh lớp chuyên hứng thú với AI hơn")
+
