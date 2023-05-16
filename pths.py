34d2c39ff1faed7086c1b3863b1e90e78e18e4fe
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 21:31:10 +0700

    Update pths.py

--- pths.py
+++ pths.py
@@ -36,21 +36,21 @@ def student():
         }
 
     df['GRADE'] = df['CLASS'].map(class_mapping1)
-    def pie(df,x,title):
-        fig = px.pie(df, names = x,title=title )
-        st.plotly_chart(fig)
-    def bieu_do():
-        a_names = ('PHÂN BỐ HS NAM - NỮ','PHÂN BỐ HS LỚP SÁNG - CHIỀU','PHÂN BỐ HS CHUYÊN - THƯỜNG','PHÂN BỐ HS CÁC KHỐI')
-        a = st.radio('Loại biểu đồ',a_names,horizontal=True)
-        if a=='PHÂN BỐ HS NAM - NỮ':
-            pie(df, df["GENDER"],"PHÂN BỐ HS NAM - NỮ")
-            st.write('Nam hứng thú với AI hơn')
-        if a=='PHÂN BỐ HS LỚP SÁNG - CHIỀU':
-            pie(df, df["PYTHON-CLASS"],"PHÂN BỐ HS LỚP SÁNG CHIỀU")
-            st.write('Học sinh thường chọn học AI vào buổi sáng')
-        if a=='PHÂN BỐ HS CHUYÊN - THƯỜNG':
-            pie(df, df["CLASS-GROUP"],"PHÂN BỐ HS CHUYÊN - THƯỜNG")
-            st.write('Học sinh lớp chuyên có hứng thú với AI hơn')
-        if a=='PHÂN BỐ HS CÁC KHỐI':
-            pie(df, df["GRADE"],"PHÂN BỐ HS CÁC KHỐI")
-            st.write("Học sinh lớp chuyên hứng thú với AI hơn")
+def pie(df,x,title):
+    fig = px.pie(df, names = x,title=title )
+    st.plotly_chart(fig)
+def bieu_do():
+    a_names = ('PHÂN BỐ HS NAM - NỮ','PHÂN BỐ HS LỚP SÁNG - CHIỀU','PHÂN BỐ HS CHUYÊN - THƯỜNG','PHÂN BỐ HS CÁC KHỐI')
+    a = st.radio('Loại biểu đồ',a_names,horizontal=True)
+    if a=='PHÂN BỐ HS NAM - NỮ':
+        pie(df, df["GENDER"],"PHÂN BỐ HS NAM - NỮ")
+        st.write('Nam hứng thú với AI hơn')
+    if a=='PHÂN BỐ HS LỚP SÁNG - CHIỀU':
+        pie(df, df["PYTHON-CLASS"],"PHÂN BỐ HS LỚP SÁNG CHIỀU")
+        st.write('Học sinh thường chọn học AI vào buổi sáng')
+    if a=='PHÂN BỐ HS CHUYÊN - THƯỜNG':
+        pie(df, df["CLASS-GROUP"],"PHÂN BỐ HS CHUYÊN - THƯỜNG")
+        st.write('Học sinh lớp chuyên có hứng thú với AI hơn')
+    if a=='PHÂN BỐ HS CÁC KHỐI':
+        pie(df, df["GRADE"],"PHÂN BỐ HS CÁC KHỐI")
+        st.write("Học sinh lớp chuyên hứng thú với AI hơn")
