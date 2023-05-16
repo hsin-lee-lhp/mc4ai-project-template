bfbc5627b0e909d2a8d777d0ff7b5af9e0c49f5c
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 23:13:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from graph import pie,box,histogram
 from tab0 import student_list
 from tab1 import graph,score
 from tab2 import kmeans
-from tab3 import  regression,classify,data
+from tab3 import regression,classify,data
 
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
 
