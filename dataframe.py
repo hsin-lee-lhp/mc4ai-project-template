94865028b9b910018d4080d36cf0215940359750
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 22:29:30 +0700

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,6 +1,9 @@
 import pandas as pd
+
 def dtf():
+
         df = pd.read_csv('py4ai-score.csv')
+
         df['S1'].fillna(0, inplace=True)  
         df['S2'].fillna(0, inplace=True)  
         df['S3'].fillna(0, inplace=True)  
@@ -13,68 +16,67 @@ def dtf():
         df['S10'].fillna(0, inplace=True)  
         df['BONUS'].fillna(0, inplace=True)  
         df['REG-MC4AI'].fillna('N', inplace=True)  
-
         class_mapping = {
-                '10CT1': 'Chuyên Toán',
-                '10CT2': 'Chuyên Toán',
-                '11CT1': 'Chuyên Toán',
-                '11CT2': 'Chuyên Toán',
-                '11CT3': 'Chuyên Toán',
-                '10CTIN': 'Chuyên Tin',
-                '10CL1': 'Chuyên Lý',
-                '10CL2': 'Chuyên Lý',
-                '10CA1': 'Chuyên Anh',
-                '10CA2': 'Chuyên Anh',
-                '11CA3': 'Chuyên Anh',
-                '10CV1': 'Chuyên Văn',
-                '10CV2': 'Chuyên Văn',
-                '10CSD': 'Chuyên Sử Địa',
-                '11CSD': 'Chuyên Sử Địa',
-                '10TH1': 'Tích Hợp',
-                '10TH2': 'Tích Hợp',
-                '10SN': 'Song Ngữ',
-                '10CH2': 'Chuyên Hóa',
-                '12CH1': 'Chuyên Hóa',
-                '10CTRN': 'Chuyên Trung Nhật',
-                '10A1': 'Khác',
-                '10A2': 'Khác',
-                '10A3': 'Khác',
-                '11A': 'Khác',
-                '11B': 'Khác',
+            '10CT1': 'Chuyên Toán',
+            '10CT2': 'Chuyên Toán',
+            '11CT1': 'Chuyên Toán',
+            '11CT2': 'Chuyên Toán',
+            '11CT3': 'Chuyên Toán',
+            '10CTIN': 'Chuyên Tin',
+            '10CL1': 'Chuyên Lý',
+            '10CL2': 'Chuyên Lý',
+            '10CA1': 'Chuyên Anh',
+            '10CA2': 'Chuyên Anh',
+            '11CA3': 'Chuyên Anh',
+            '10CV1': 'Chuyên Văn',
+            '10CV2': 'Chuyên Văn',
+            '10CSD': 'Chuyên Sử Địa',
+            '11CSD': 'Chuyên Sử Địa',
+            '10TH1': 'Tích Hợp',
+            '10TH2': 'Tích Hợp',
+            '10SN': 'Song Ngữ',
+            '10CH2': 'Chuyên Hóa',
+            '12CH1': 'Chuyên Hóa',
+            '10CTRN': 'Chuyên Trung Nhật',
+            '10A1': 'Thường',
+            '10A2': 'Thường',
+            '10A3': 'Thường',
+            '11A': 'Thường',
+            '11B': 'Thường',
 
-            }
+        }
 
         df['CLASS-GROUP'] = df['CLASS'].map(class_mapping)
 
 
         class_mapping1 = {
-                '10CT1': 'Lớp 10',
-                '10CT2': 'Lớp 10',
-                '11CT1': 'Lớp 11',
-                '11CT2': 'Lớp 11',
-                '11CT3': 'Lớp 11',
-                '10CTIN': 'Lớp 10',
-                '10CL1': 'Lớp 10',
-                '10CL2': 'Lớp 10',
-                '10CA1': 'Lớp 10',
-                '10CA2': 'Lớp 10',
-                '11CA3': 'Lớp 11',
-                '10CV1': 'Lớp 10',
-                '10CV2': 'Lớp 10',
-                '10CSD': 'Lớp 10',
-                '11CSD': 'Lớp 11',
-                '10TH1': 'Lớp 10',
-                '10TH2': 'Lớp 10',
-                '10SN': 'Lớp 10',
-                '10CH2': 'Lớp 10',
-                '12CH1': 'Lớp 12',
-                '10CTRN': 'Lớp 10',
-                '10A1': 'Lớp 10',
-                '10A2': 'Lớp 10',
-                '10A3': 'Lớp 10',
-                '11A': 'Lớp 11',
-                '11B': 'Lớp 11',
+            '10CT1': 'Lớp 10',
+            '10CT2': 'Lớp 10',
+            '11CT1': 'Lớp 11',
+            '11CT2': 'Lớp 11',
+            '11CT3': 'Lớp 11',
+            '10CTIN': 'Lớp 10',
+            '10CL1': 'Lớp 10',
+            '10CL2': 'Lớp 10',
+            '10CA1': 'Lớp 10',
+            '10CA2': 'Lớp 10',
+            '11CA3': 'Lớp 11',
+            '10CV1': 'Lớp 10',
+            '10CV2': 'Lớp 10',
+            '10CSD': 'Lớp 10',
+            '11CSD': 'Lớp 11',
+            '10TH1': 'Lớp 10',
+            '10TH2': 'Lớp 10',
+            '10SN': 'Lớp 10',
+            '10CH2': 'Lớp 10',
+            '12CH1': 'Lớp 12',
+            '10CTRN': 'Lớp 10',
+            '10A1': 'Lớp 10',
+            '10A2': 'Lớp 10',
+            '10A3': 'Lớp 10',
+            '11A': 'Lớp 11',
+            '11B': 'Lớp 11',
 
-            }
+        }
 
         df['GRADE'] = df['CLASS'].map(class_mapping1)
