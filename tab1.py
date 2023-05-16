b4ccc861478039547798a9cc7cac782a34610d78
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 21:42:37 +0700

    Update tab1.py

--- tab1.py
+++ tab1.py
@@ -49,38 +49,6 @@ def score():
         histogram(df,'GPA',None,'GENDER')
         pie(df[df['GPA'] >= 6],'CLASS-GROUP','Tỉ lệ đậu lớp MC')
 
-def student():
-    class_mapping1 = {
-            '10CT1': 'Lớp 10',
-            '10CT2': 'Lớp 10',
-            '11CT1': 'Lớp 11',
-            '11CT2': 'Lớp 11',
-            '11CT3': 'Lớp 11',
-            '10CTIN': 'Lớp 10',
-            '10CL1': 'Lớp 10',
-            '10CL2': 'Lớp 10',
-            '10CA1': 'Lớp 10',
-            '10CA2': 'Lớp 10',
-            '11CA3': 'Lớp 11',
-            '10CV1': 'Lớp 10',
-            '10CV2': 'Lớp 10',
-            '10CSD': 'Lớp 10',
-            '11CSD': 'Lớp 11',
-            '10TH1': 'Lớp 10',
-            '10TH2': 'Lớp 10',
-            '10SN': 'Lớp 10',
-            '10CH2': 'Lớp 10',
-            '12CH1': 'Lớp 12',
-            '10CTRN': 'Lớp 10',
-            '10A1': 'Lớp 10',
-            '10A2': 'Lớp 10',
-            '10A3': 'Lớp 10',
-            '11A': 'Lớp 11',
-            '11B': 'Lớp 11',
-
-        }
-
-    df['GRADE'] = df['CLASS'].map(class_mapping1)
 def pie(df,x,title):
     fig = px.pie(df, names = x,title=title )
     st.plotly_chart(fig)
