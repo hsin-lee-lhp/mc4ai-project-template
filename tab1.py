017210356caa3b71ed043555839433325314e83c
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 22:45:56 +0700

    Update tab1.py

--- tab1.py
+++ tab1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from graph import box,histogram,pie
 from dataframe import *
-dtf()
+
 def score():                                
     s_names = ('S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','GPA')
     s = st.radio('Điểm từng session',s_names,horizontal=True)
@@ -50,7 +50,8 @@ def score():
         histogram(df,'GPA',None,'GENDER')
         pie(df[df['GPA'] >= 6],'CLASS-GROUP','Tỉ lệ đậu lớp MC')
 
-def bieu_do():
+def graph():
+    dtf()
     a_names = ('PHÂN BỐ HS NAM - NỮ','PHÂN BỐ HS LỚP SÁNG - CHIỀU','PHÂN BỐ HS CHUYÊN - THƯỜNG','PHÂN BỐ HS CÁC KHỐI')
     a = st.radio('Loại biểu đồ',a_names,horizontal=True)
     if a=='PHÂN BỐ HS NAM - NỮ':
