fb1f681f3c48070e204f09489aae948e44e80253
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-16 21:46:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import streamlit as st
 import pandas as pd
 from dataframe import df
 from graph import *
-from tab0 import student_list
-from tab1 import score, student
-from tab2 import kmeans
-from tab3 import regression,classify
+from tab0 import *
+from tab1 import *
+from tab2 import *
+from tab3 import *
 
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
-
 
 tabs_titles = ('Danh sách',
                'Biểu đồ',
@@ -18,15 +17,14 @@ tabs = st.tabs(tabs_titles)
 
 with tabs[0]:
     student_list()
-    
-    
-   
+      
 with tabs[1]:
     tabs1_titles = ('Số lượng hs',
                     'Điểm')
     tabs1 = st.tabs(tabs1_titles)
     with tabs1[0]:
         student()
+        bieu_do()
     with tabs1[1]:
         score()
 
