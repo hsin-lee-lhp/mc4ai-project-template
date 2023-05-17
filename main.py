67112a6c1960c4660ad22094ec2c12c86f8c021b
peekaboo <125679435+hsin-lee-lhp@users.noreply.github.com>
2023-05-17 19:15:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from graph import pie,box,histogram
 from tab0 import student_list
 from tab1 import graph,score
 from tab2 import kmeans
-from tab3 import dt
+from tab3 import regression,data,create_data
 
 st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
 
@@ -30,7 +30,9 @@ with tabs[1]:
 with tabs[2]:
     kmeans()
 with tabs[3]:
-    dt()
+    regression()
+    create_data()
+    data()
     
     
    
